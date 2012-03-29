34e0a006f11993cf3ba332333d4f53da8c953a39
Antonio Carlos <antonio@tauoca.c3local>
2012-03-29 19:32:54 -0300

    Modified calibration script.

---
+++ scripts/Calibration.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/ruby
+
+
+
+module OpenDC
+	class Calibration
+
+
+	    DEFAULT_HOST = "localhost"
+	    DEFAULT_PORT = 5432
+	    DEFAULT_DBNAME = "calibration"
+	    DEFAULT_DBUSER = "calibrate"
+	    DEFAULT_DBPASSWORD = "calibrate"
+	#    DEFAULT_QUERY_FOLDER = "./queries"
+
+
+
+	    FILE_MAX_CONTENT = 90000
+
+
+	    def initialize( host = DEFAULT_HOST, port = DEFAULT_PORT, dbname = DEFAULT_DBNAME, dbuser = DEFAULT_DBUSER, dbpassword = DEFAULT_DBPASSWORD)
+
+#		@conn = PG.connect( :host => host, :dbname => dbname, :port => port, :user => dbuser, :password => dbpassword ) or
+#		            abort "Unable to create a new connection!"
+
+		@conn = PG.connect( :host => host, :dbname => dbname, :port => port, :user => dbuser, :password => dbpassword ) 
+
+		unless @conn
+			raise Error "Couldn't connect to specified database" 
+		end
+
+
+	    end
+
+	    def get_costs(query_file)
+
+		fp = File.open(query_file,"r")
+
+		unless fp
+		    Error "Unable to open file #{query_file}."
+		end
+
+		costs = Array.new
+		result = run_analyze_query(fp.sysread(FILE_MAX_CONTENT))
+		puts result.values
+		result.each{ |tuple|
+		    new_cost = get_actual_total_cost(tuple['QUERY PLAN'])
+			costs << new_cost if new_cost
+		}
+		costs
+	    end
+
+	private
+	    def run_analyze_query(raw_query)
+		query_text = "EXPLAIN ANALYZE "+raw_query;
+		@conn.exec(query_text)
+	    end
+
+	    def get_actual_total_cost(tuple)
+		match = tuple[/actual time=([0-9]*\.[0-9]+|[0-9]+)\.\.([0-9]*\.[0-9]+|[0-9]+)/, 2]
+		    if match
+		    match.to_f
+		else
+		    nil
+		end
+	    end
+
+	end
+
+end
+
+
+
+#cal = Calibration.new
+#puts cal.get_costs("./queries/seq_scan.sql")
+#cal.get_costs("./queries/aggregate_seq_scan.sql")
+
--- scripts/host_db_calibration.rb
+++ scripts/host_db_calibration.rb
@@ -6,6 +6,9 @@
 
 ONE_LOCATION=ENV["ONE_LOCATION"]
 
+DEPLOY_TIMEOUT=10
+DEPLOY_CHECK=4
+
 if !ONE_LOCATION
     RUBY_LIB_LOCATION="/usr/lib/one/ruby"
     VMDIR="/var/lib/one"
@@ -23,9 +26,10 @@ require 'OpenDC'
 include OpenDC
 
 if !(host_id=ARGV[0])
+    puts "No host informed"
     exit -1
 end
-
+1
 begin
     client = Client.new()
 rescue Exception => e
@@ -42,33 +46,68 @@ host_name = host.name
 # Search for Image Template
 images = ImagePool.new(client)
 exit -1 if OpenNebula.is_error?(images)
+images.info_all
+image_ids_array = images.retrieve_elements("/IMAGE_POOL/IMAGE[NAME='CentOS 6 with PostgreSQL databases']/ID")
+
+
+unless image_ids_array
+	puts "Couldn't find calibration image."
+	exit(-1)
+end
+
+image = OpenNebula::Image.new_with_id(image_ids_array.first,client)
+
+
+image.info
+
+state = OpenNebula::Image::IMAGE_STATES[image.state]
+if  state == 'DISABLED'
+	image.enable
+end
 
 
 
+# Loop through all templates
+templates = TemplatePool.new(client)
+exit -1 if OpenNebula.is_error?(templates)
+
+templates.info_all
+template_ids_array = templates.retrieve_elements("/VMTEMPLATE_POOL/VMTEMPLATE/TEMPLATE/DISK[IMAGE_ID=\"#{image.id}\"]/../../ID")
 
 
+if template_ids_array
+    template_ids_array.each do |template_id|
+    	template = OpenNebula::Template.new_with_id(template_id, client)
+    	template.info
 
+        vm_id=template.instantiate("calib-host:#{host_id}")
+    	exit -1 if OpenNebula.is_error?(vm_id)
 
-# Loop through all vms
-vms = VirtualMachinePool.new(client)
-exit -1 if OpenNebula.is_error?(vms)
-
-vms.info_all
-
-state = "STATE=3"
-state += " or STATE=5" if force == "y"
-
-vm_ids_array = vms.retrieve_elements("/VM_POOL/VM[#{state}]/HISTORY_RECORDS/HISTORY[HOSTNAME=\"#{host_name}\" and last()]/../../ID")
-
-if vm_ids_array
-    vm_ids_array.each do |vm_id|
-        vm=OpenNebula::VirtualMachine.new_with_id(vm_id, client)
+    	vm = OpenNebula::VirtualMachine.new_with_id(vm_id)
         vm.info
 
-        if mode == "-r"
-            vm.resubmit
-        elsif mode == "-d"
-            vm.finalize
-        end
+        vm.hold
+
+    	vm.deploy(host_id)
+
+    	sleep DEPLOY_TIMEOUT.seconds
+    	att = 1
+    	vm.info
+    	while (  OpenNebula::VM_STATE[vm.state] != 'ACTIVE'   )
+    		if  ( att >= DEPLOY_CHECK )
+    			puts "Couldn't deploy VM."
+    			exit -1;
+    		end
+            if ( OpenNebula::VM_STATE[vm.state] == 'FAILED' )
+                puts "Deployment failed."
+            end
+    		att+=1
+            sleep DEPLOY_TIMEOUT.seconds
+            vm.info
+    	end
+
     end
 end
+
+
+exit 0
