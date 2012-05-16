ba1b2bd03ba5584706c2f85a3b1093f68061c8e5
Antonio <marcojrfurtado@gmail.com>
2012-05-16 17:01:50 -0300

    OpenNebula version I am modifying.

---
+++ dist/.xmlrpc_test/xmlrpc_test.a4210c.cc
@@ -0,0 +1,9 @@
+
+#include <xmlrpc-c/base.hpp>
+#include <xmlrpc-c/registry.hpp>
+
+int main(int argc, char *argv[])
+{
+    xmlrpc_c::registry RequestManagerRegistry;
+    return 0;
+}
---
+++ dist/src/opendc/CPUResourceController.c
@@ -0,0 +1,1 @@
+#include <libcgroup.h>
